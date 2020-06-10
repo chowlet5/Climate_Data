36298f28786643b39584b4f0598c5c3a6bed1ef7
chowlet5 <howlett2306@rogers.com>
2020-06-10 19:33:59 -0400

    Update getHourlyData function
    The data format accessed using the API endpoint doesm't contain the 15 line header. Therefore, skiprows has been set to 0.

--- GoC_Historical_Download_Script.py
+++ GoC_Historical_Download_Script.py
@@ -2,7 +2,10 @@ import pandas as pd
 import datetime
 from dateutil import rrule
 from datetime import datetime, timedelta
-
+from bs4 import BeautifulSoup
+import requests
+import re
+#from fuzzywuzzy import fuzz
 
 # Call Environment Canada API
 # Returns a dataframe of data
@@ -10,13 +13,13 @@ def getHourlyData(stationID, year, month):
     base_url = "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?"
     query_url = "format=csv&stationID={}&Year={}&Month={}&timeframe=1".format(stationID, year, month)
     api_endpoint = base_url + query_url
-    return pd.read_csv(api_endpoint, skiprows=15)
+    return pd.read_csv(api_endpoint, skiprows=0)
 
 
 
-stationID = 50093
-start_date = datetime.strptime('Mar2012', '%b%Y')
-end_date = datetime.strptime('Apr2019', '%b%Y')
+stationID = 5097
+start_date = datetime.strptime('Jan1953', '%b%Y')
+end_date = datetime.strptime('Jun2019', '%b%Y')
 
 frames = []
 for dt in rrule.rrule(rrule.MONTHLY, dtstart=start_date, until=end_date):
@@ -27,5 +30,6 @@ weather_data = pd.concat(frames)
 weather_data['Date/Time'] = pd.to_datetime(weather_data['Date/Time'])
 weather_data['Temp (°C)'] = pd.to_numeric(weather_data['Temp (°C)'])
 
+output_folder= r'climate_data/SWL'
 
-weather_data.to_csv('LondonA 2012-2019.csv')
+weather_data.to_csv(rf'{output_folder}/Pearson 1953-2013.csv')
